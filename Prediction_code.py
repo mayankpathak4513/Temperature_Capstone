4e6a457f6556b63995305e8762bce511d078b977
Mayank Pathak <mayankpathak4513@gmail.com>
2021-06-02 20:37:12 +0530

    Created Prediction file
    Created Prediction file which can predicts the Future instance values

---
+++ Prediction_code.py
@@ -0,0 +1,15 @@
+#This code helps in Predicting future instance values.
+
+# Shows up the different Plots and can able to make comparisions.
+
+setChartLibrary('google-chart');
+setChartTitle('Polynomial Regression');
+setChartType('predictionGraph');
+setAxisName('time_stamp','temp');
+mul(0.097);
+setAnimation(true);
+setCrosshair(true);
+plotChart('time_stamp','analog');
+
+
+
