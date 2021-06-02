8efef9e4a6fb2217f0a8bbfd985313a89192553b
Mayank Pathak <mayankpathak4513@gmail.com>
2021-06-02 20:32:44 +0530

    Created Config file
    Created the Configuration file to configure the different sets of devices and platform

---
+++ config.py
@@ -0,0 +1,10 @@
+#This code is used to config the Twillio with the device
+#configurations and credential file
+
+api_key = 'XXXXXX' # Enter your API Key used in Twillio App
+device_id = 'BOLTXXXXXXX' # Enter the device id i.e. the Bolt Id
+telegram_chat_id = '@XXXXXXX'
+telegram_bot_id = 'botXXXXXXXX'
+threshold = [206.186, 309.28, 412.38] #Change the threshold values according to your environment
+frame_size = 10
+mul_factor = 2
